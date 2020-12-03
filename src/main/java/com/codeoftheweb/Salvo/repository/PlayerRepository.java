67527fcc475264bcad452a3e58792e1770062e27
Brimix <brian.morris.e97@gmail.com>
2020-12-03 08:47:42 -0300

    Added findByEmail

--- src/main/java/com/codeoftheweb/Salvo/repository/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/Salvo/repository/PlayerRepository.java
@@ -1,7 +1,5 @@
 package com.codeoftheweb.Salvo.repository;
 
-import java.util.List;
-
 import com.codeoftheweb.Salvo.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-//    List<Player> findByEmail(String email);
-    Player findByEmail(@Param("name") String name);
-
+    Player findByEmail(@Param("email") String email);
 }
